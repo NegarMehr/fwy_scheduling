ee95b2708e22f99250946b0fcae549f2be1c5fad
NegarZahedimehr <negar_8270@yahoo.com>
2016-08-15 19:41:06 -0700

    noninterative control works whereas iterative one is unstable

---
+++ nonIterativeControl.m
@@ -0,0 +1,70 @@
+% x is the primary variable of optimization problem
+% alpha's are the prices
+% n : mainline density
+% l : onramp density
+% f : mainline flows
+% r : onramp flows 
+clear all;
+close all;
+clc;
+% network data
+simpleNetworkStruct;
+%% initial condition
+
+n0 = [15;15];
+l0 = [3;3];
+%% setting up the optimization
+
+n_seg = size(params.v,1);
+n_or = size(find(params.has_or),1);
+n_cur = n0;
+l_cur = l0;
+max_iter = 100;
+% preallocation
+x = zeros(n_or,max_iter+1);
+x0 = [1;1];
+x(:,1) = x0;
+
+n = zeros(n_seg,max_iter+1);
+l = zeros(n_seg, max_iter+1);
+f = zeros(n_seg, max_iter);
+r = zeros(n_seg, max_iter);
+n(:,1) = n0;
+l(:,1) = l0;
+%% iterative control
+for iter = 1:max_iter
+    [A, b] = optMatrices(params, n_cur);
+    x_cur = x(:,iter);
+%     if ~isempty((A*x_cur - b) > 0)
+%         error('infeasible initial condition')
+%     end
+%     x_cur = r_cur + 0.01*ones(n_or,1);
+    fun = @(x) -log(x(1)) - log(x(2)); 
+    x_next = fmincon(fun,[1;1],A,b);
+    % storage
+    x(:,iter+1) = x_next;
+    % control input
+    r_cur = min(x_next, params.r_bar);
+    r_cur = max(r_cur,zeros(n_or,1));
+    % evolve model
+    [n_next, l_next, f_cur] = fwyDynamics(n_cur, l_cur, r_cur, params);
+    % storage
+    n(:,iter+1) = n_next;
+    l(:,iter+1) = l_next;
+    f(:,iter) = f_cur;
+    r(:,iter) = r_cur;
+    % updating current density
+    n_cur = n_next;
+    l_cur = l_next;
+end
+%% plotting
+figure('name','n');
+plot(n');
+figure('name','l');
+plot(l');
+figure('name','f');
+plot(f');
+figure('name','r');
+plot(r');
+figure('name','x');
+plot(x');
---
+++ optMatrices.m
@@ -0,0 +1,47 @@
+function [A, b] = optMatrices(params, n_cur)
+%% This function returns the linear inequality matrices of constraints
+% extracting parameters
+w = params.w;
+f_bar = params.f_bar;
+n_bar = params.n_bar;
+beta_bar = 1 - params.beta;
+ml_d = params.d_up;
+has_or = params.has_or;
+%% preallocating matrices
+n_seg = size(has_or,1);
+or_ind = find(has_or);
+n_or = size(or_ind,1);
+A = zeros(n_seg*2-1,n_or);
+b = zeros(n_seg*2-1,1);
+b_d = zeros(n_seg*2-1,1);
+%% Constructing matrices
+or_count = 0;
+iter_row = zeros(2,n_or);
+for iter = 1:n_seg-1
+    row_ind = 2*(iter-1)+1;
+    seg_ind = ceil(iter/2);
+    %% A matrix
+    % new block
+    new_row = iter_row*beta_bar(seg_ind);
+    A(row_ind:row_ind+1,:) = new_row;
+    if has_or(seg_ind)
+        or_count = or_count + 1;
+        A(row_ind,or_count) = beta_bar(seg_ind);
+        A(row_ind+1,or_count) = beta_bar(seg_ind);
+    end
+    %% b_d
+    b_d(row_ind:row_ind+1,:) = [prod(beta_bar(1:seg_ind,1))*ml_d(1);prod(beta_bar(1:seg_ind,1))*ml_d(1)];
+    b(row_ind:row_ind+1,:) = [f_bar(seg_ind);w(seg_ind+1)*(n_bar(seg_ind+1)-n_cur(seg_ind+1))];
+end
+A(end,:) = beta_bar(end)*A(end-2,:);
+if has_or(end)
+    or_count = or_count + 1;
+    A(end,or_count) = beta_bar(end);
+end
+%% b_d
+b_d(end,:) = prod(beta_bar(1:seg_ind,1))*ml_d(1);
+b(end) = f_bar(end);
+b = b - b_d;
+
+
+
